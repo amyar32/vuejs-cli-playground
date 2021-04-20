8795e5004aa536097a039903494a0da92738eb1c
A. Y. Firdaus <amyar32@gmail.com>
2021-04-20 18:57:15 +0700

    fixing bug in prod

--- src/main.js
+++ src/main.js
@@ -1,10 +1,9 @@
 import { createApp } from "vue";
 import App from "./App.vue";
-import "tailwindcss/tailwind.css";
+import "./index.css";
 import Navbar from "./components/Navbar";
 import Hisab from "./components/Hisab";
 import JadwalList from "./components/JadwalList";
-// import TodoList from "./components/TodoList";
 import DarkModeButton from "./components/DarkModeButton";
 
 const app = createApp(App);
@@ -12,7 +11,6 @@ const app = createApp(App);
 app.component("nav-bar", Navbar);
 app.component("hisab", Hisab);
 app.component("jadwal-list", JadwalList);
-// app.component("todo-list", TodoList);
 app.component("dark-toggler", DarkModeButton);
 
 app.mount("#app");
