f712de033e28a7af383ab55ca6542f3a0198780a
A. Y. Firdaus <amyar32@gmail.com>
2021-04-20 18:13:16 +0700

    improve styling

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="darkMode">
     <div
-      class="h-screen w-screen bg-blue-500 dark:bg-gray-800 transition duration-400 ease-out overflow-auto absolute"
+      class="h-screen w-screen bg-gray-100 dark:bg-gray-800 transition duration-400 ease-out overflow-auto absolute"
     >
       <hisab></hisab>
-      <dark-toggler
+      <!-- <dark-toggler
         :isDark="isDark"
         message="Mode"
         @toggle-dark="toggleDark"
-      ></dark-toggler>
+      ></dark-toggler> -->
       <todo-list
         title="Holy Todo List"
         sub-title="Todo list app by using VueJs and Tailwind CSS"
@@ -21,7 +21,7 @@
 export default {
   data() {
     return {
-      isDark: true,
+      isDark: false,
     };
   },
   computed: {
