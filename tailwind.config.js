033924796a86c3f72876f704ccb6cff8d807a8eb
A. Y. Firdaus <amyar32@gmail.com>
2021-04-20 18:12:51 +0700

    add new collor pallete

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,13 @@ module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.js"],
   darkMode: "class", // or 'media' or 'class'
   theme: {
+    extend: {
+      colors: {
+        manis: "#642B73",
+        jomblo: "#C6426E",
+      },
+    },
+
     container: {
       center: true,
     },
