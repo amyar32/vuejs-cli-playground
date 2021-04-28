77c4b5b966a616a78b2a5017f6e03d0f03cf65e3
A. Y. Firdaus <amyar32@gmail.com>
2021-04-28 12:02:31 +0700

    cleaning up

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,14 @@
 <template>
   <div :class="darkMode">
+    <nav-bar></nav-bar>
     <div
-      class="h-screen w-screen bg-gray-100 dark:bg-gray-800 transition duration-400 ease-out overflow-auto absolute"
+      class="h-screen w-screen bg-gray-100 dark:bg-black transition duration-400 ease-out overflow-auto"
     >
-      <hisab></hisab>
-      <!-- <dark-toggler
+      <dark-toggler
         :isDark="isDark"
         message="Mode"
         @toggle-dark="toggleDark"
-      ></dark-toggler> -->
+      ></dark-toggler>
       <todo-list
         title="Holy Todo List"
         sub-title="Todo list app by using VueJs and Tailwind CSS"
