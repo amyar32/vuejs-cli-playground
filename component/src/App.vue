b25601e821d64788af1f3ac8fc4102409df3e78e
A. Y. Firdaus <amyar32@gmail.com>
2021-05-22 15:31:00 +0700

    new life

--- component/src/App.vue
+++ component/src/App.vue
@@ -5,18 +5,22 @@
       class="h-screen w-screen bg-gray-100 dark:bg-black transition duration-400 ease-out overflow-auto"
     >
       <!-- <dark-toggler message="Mode" @toggle-dark="toggleDark"></dark-toggler> -->
-      <parent></parent>
+      <!-- <parent></parent> -->
 
       <!-- penggunaan dynamic component -->
-      <dynamic-component></dynamic-component>
+      <!-- <dynamic-component></dynamic-component> -->
+
+      <!-- form example di vue -->
+      <!-- <forms></forms> -->
     </div>
   </div>
 </template>
 
 <script>
 import DynamicComponent from "./components/DynamicComponent";
+import Forms from "./components/forms/form";
 export default {
-  components: { DynamicComponent },
+  components: { DynamicComponent, Forms },
   data() {
     return {
       isDark: false,
---
+++ http-request/src/components/survey/UserExperiences.vue
@@ -0,0 +1,75 @@
+<template>
+  <section>
+    <base-card>
+      <h2>Submitted Experiences</h2>
+      <div>
+        <base-button @click="loadExperiences"
+          >Load Submitted Experiences</base-button
+        >
+      </div>
+      <p v-if="isLoading">Loading</p>
+      <ul v-else>
+        <survey-result
+          v-for="result in results"
+          :key="result.id"
+          :name="result.name"
+          :rating="result.rating"
+        ></survey-result>
+      </ul>
+    </base-card>
+  </section>
+</template>
+
+<script>
+import BaseButton from '../UI/BaseButton.vue';
+import SurveyResult from './SurveyResult.vue';
+
+export default {
+  components: {
+    SurveyResult,
+    BaseButton
+  },
+  data() {
+    return {
+      results: [],
+      isLoading: false
+    };
+  },
+  methods: {
+    loadExperiences() {
+      this.isLoading = true;
+      fetch(
+        'https://vue-http-demo-99a21-default-rtdb.asia-southeast1.firebasedatabase.app/surveys.json'
+      )
+        .then(response => {
+          if (response.ok) {
+            return response.json();
+          }
+        })
+        .then(data => {
+          this.isLoading = false;
+          const results = [];
+          for (const id in data) {
+            results.push({
+              id: id,
+              name: data[id].name,
+              rating: data[id].rating
+            });
+          }
+          this.results = results;
+        });
+    }
+  },
+  mounted() {
+    this.loadExperiences();
+  }
+};
+</script>
+
+<style scoped>
+ul {
+  list-style: none;
+  margin: 0;
+  padding: 0;
+}
+</style>
