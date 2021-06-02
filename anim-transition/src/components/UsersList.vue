34c911e020928b7393221a1bcdb86e59b83c8894
A. Y. Firdaus <amyar32@gmail.com>
2021-06-02 18:11:09 +0700

    added pog list transition

--- anim-transition/src/components/UsersList.vue
+++ anim-transition/src/components/UsersList.vue
@@ -1,9 +1,11 @@
 <template>
-  <ul>
-    <li v-for="user in users" :key="user" @click="removeUser(user)">
+  <!-- list animate is here  -->
+  <transition-group tag="ul" name="user-list"
+    ><li v-for="user in users" :key="user" @click="removeUser(user)">
       {{ user }}
-    </li>
-  </ul>
+    </li></transition-group
+  >
+
   <div>
     <input type="text" ref="userNameInput" />
     <button @click="addUser">Add</button>
@@ -39,5 +41,31 @@ li {
   border: 1px solid red;
   padding: 1rem;
   text-align: center;
+}
+
+.user-list-enter-from,
+.user-list-leave-to {
+  opacity: 0;
+}
+
+.user-list-enter-active {
+  transition: all 0.2s ease-out;
+}
+
+.user-list-leave-active {
+  transition: all 0.2s ease-in;
+  position: absolute;
+}
+
+.user-list-enter-to,
+.user-list-leave-from {
+  opacity: 1;
+}
+
+/* pog  */
+/* transform under the hood by vue */
+
+.user-list-move {
+  transition: transform 0.8s ease-in;
 }
 </style>
